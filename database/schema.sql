0eaefee46a32445c17b31bb898ef19ff79855a72
moaz ashraf <101815877+Moaz-ashraf1@users.noreply.github.com>
2025-11-19 02:06:44 +0200

    Create tables for Category, Product, Customer, and Orders

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,45 @@
+CREATE TABLE Category (
+    category_id INTEGER,
+    category_name VARCHAR(50),
+    PRIMARY KEY (category_id)
+);
 
+CREATE TABLE Product (
+    product_id INTEGER,
+    category_id INTEGER NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    price NUMERIC(5,2) NOT NULL CHECK (price >= 0),
+    stock_quantity INTEGER NOT NULL CHECK (stock_quantity >= 0),
+    PRIMARY KEY (product_id),
+    FOREIGN KEY (category_id) REFERENCES Category(category_id)
+);
+
+CREATE TABLE Customer (
+    customer_id INTEGER,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE Orders (
+    order_id INTEGER,
+    customer_id INTEGER NOT NULL,
+    order_date DATE NOT NULL,
+    total_amount NUMERIC(10,2) NOT NULL DEFAULT 0,
+    PRIMARY KEY (order_id),
+    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+);
+
+CREATE TABLE Order_details (
+    order_detail_id INTEGER,
+    order_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    quantity INTEGER NOT NULL CHECK (quantity > 0),
+    unit_price NUMERIC(5,2) NOT NULL CHECK (unit_price >= 0),
+    PRIMARY KEY (order_detail_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES Product(product_id)
+);
